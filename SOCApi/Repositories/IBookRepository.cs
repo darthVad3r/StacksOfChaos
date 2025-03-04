8a59b35cbdb345d6551f13ef4cbb9fef00cb440d
Bobby Finley <rmfinley@yahoo.com>
2025-03-04 13:15:33 -0500

    Update launch settings, SOCApi, and add IBookRepository
    Updated launchSettings.json to auto-launch browser and change URLs.
    Modified SOCApi.http to update host address and endpoint.
    Added IBookRepository interface with CreateTitleAsync and DeleteTitleAsync methods.

---
+++ SOCApi/Repositories/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using SOCApi.Models;
+
+namespace SOCApi.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<Title?> CreateTitleAsync(Title title);
+        Task<bool?> DeleteTitleAsync(int id);
+    }
+}
