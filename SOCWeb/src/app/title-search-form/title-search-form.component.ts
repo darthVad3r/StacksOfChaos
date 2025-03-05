59fd31ea798608e12c7b55056bddd66114705100
darthVad3r <rmfinley@yahoo.com>
2025-03-04 22:38:13 -0500

    Enhance title-search-form component: add required attribute to input, display search results in a list, and improve formatting

--- SOCWeb/src/app/title-search-form/title-search-form.component.ts
+++ SOCWeb/src/app/title-search-form/title-search-form.component.ts
@@ -1,30 +1,35 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
 import { TitleSearchService } from './title-search-service';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-title-search-form',
   templateUrl: './title-search-form.component.html',
   styleUrls: ['./title-search-form.component.css'],
-  imports: [FormsModule]
+  imports: [FormsModule],
 })
 export class TitleSearchFormComponent {
   inputText: string = '';
   titles: string[] = [];
 
-  constructor(@Inject(TitleSearchService) private titleSearchService: TitleSearchService) {}
+  constructor(
+    @Inject(TitleSearchService) private titleSearchService: TitleSearchService
+  ) {}
 
   searchForTitle() {
     alert(this.inputText);
-    console.log("Hello World");
+    console.log('Hello World');
     this.titleSearchService.searchTitles(this.inputText).subscribe({
       next: (response: { titles: string[] }) => {
         this.titles = response.titles;
       },
       error: (error: HttpErrorResponse) => {
-        console.error(`Error occurred while searching for "${this.inputText}": `, error.message);
-      }
+        console.error(
+          `Error occurred while searching for "${this.inputText}": `,
+          error.message
+        );
+      },
     });
   }
 }
