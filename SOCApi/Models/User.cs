6cdeddaf5ecf443decbc7182cd81d9b71a7c15b8
Bobby Finley <rmfinley@yahoo.com>
2025-03-12 16:47:29 -0400

    Add Username property and update stored procedures
    - Add `Username` property to `User` class in `User.cs`
    - Add `Username` property with validation to `LoginRequest` class
    - Rename `AddNewUser.sql` to `usp_AddNewUser.sql` in project file
    - Remove old `AddNewUser.sql` and add new `usp_AddNewUser.sql`
    - Update `Users` table to include `UserName` column
    - Create new `usp_AddNewUser.sql` to insert user with `UserName`

--- SOCApi/Models/User.cs
+++ SOCApi/Models/User.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Token { get; set; } = string.Empty;
--- SOCApi/ViewModels/LoginRequest.cs
+++ SOCApi/ViewModels/LoginRequest.cs
@@ -8,9 +8,14 @@ namespace SOCApi.ViewModels
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "Password is required")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
         [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Username must be at least 6 characters long")]
+        public string Username { get; set; } = string.Empty;
     }
 }
